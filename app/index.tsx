a062e23decf9f4880347cf7325146964a1549155
quinnhchapman <quinnhchapman@gmail.com>
2025-12-03 22:48:11 -0500

    API Call + API Key

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,51 @@
 import { Text, View } from "react-native";
-import { getWeather, dailyForecast, showWeather, getLocation } from 'react-native-weather-api';
+import React, {useState, useEffect} from "react";
+import * as Location from 'expo-location';
 
 // https://github.com/pierreamgabriel/react-native-weather-api
 
+
 export default function Index() {
+  const [temperature, setTemp] = useState(0);
+  const [city, setCity] = useState(null);
+  const [clothing, setClothing] = useState(null);
+
+
+useEffect(() => {
+
+  Location.requestForegroundPermissionsAsync().then(
+      (permission: Location.PermissionResponse) => {
+        if (permission.status !== "granted") {
+          console.log("Permission denied");
+          return;
+        }
+
+    Location.getCurrentPositionAsync({}).then(
+          (location: Location.LocationObject) => {
+            const { latitude, longitude } = location.coords;
+
+            const apiKey = "694a9a1429a0eb720906af6a6c3661b1";
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+
+
+            
+            fetch(url)
+              .then((response) => response.json())
+              .then((data) => {
+                setTemp(data.main.temp);
+                
+              });
+          }
+        );
+      }
+    );
+  }, []);
+
+
+    
+
+
+
   return (
     <View
       style={{
@@ -12,7 +54,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Weather App</Text>
+      <Text>Current Temperature: {temperature} </Text>
+      <Text>You should grab a {clothing} today!</Text>
     </View>
   );
 }
