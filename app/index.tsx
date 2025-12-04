70d0682e017ba4bf57f232d73eb1bdafb42538d2
AdamDBWork <adamdb.work@gmail.com>
2025-12-04 01:58:31 -0500

    added date, time, and comments

--- app/index.tsx
+++ app/index.tsx
@@ -6,11 +6,42 @@ import * as Location from 'expo-location';
 
 
 export default function Index() {
+
+  // Weather Constants
   const [temperature, setTemp] = useState(0);
   const [city, setCity] = useState(null);
   const [clothing, setClothing] = useState(null);
 
+  // Time Constants
+  const weekday = new Date().toLocaleDateString("en-US", {weekday: "long"});
+  const day = new Date().toLocaleDateString("en-US", {day: "numeric"});
+  const month = new Date().toLocaleDateString("en-US", {month: "long"});
+   
+  const time = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric"}
+  );
 
+// getSuffix Function
+  // This ensures the number day has the right suffix (ex; 3rd instead of 3th)
+function getSuffix(num) {
+
+  const lastDigit = num % 10;
+  const lastTwo = num % 100;
+  
+  if (lastTwo >= 11 && lastTwo <=13) {
+    return num + "th";
+  }
+
+  switch (lastDigit) {
+    case 1: return num + "st";
+    case 2: return num + "nd";
+    case 3: return num + "rd";
+    default: return num + "th";
+  }
+}
+
+// Location Request
 useEffect(() => {
 
   Location.requestForegroundPermissionsAsync().then(
@@ -41,11 +72,7 @@ useEffect(() => {
     );
   }, []);
 
-
-    
-
-
-
+  // Return Statement
   return (
     <View
       style={{
@@ -54,6 +81,9 @@ useEffect(() => {
         alignItems: "center",
       }}
     >
+      <Text>{weekday}, {getSuffix(Number(day))}, {month} </Text>
+      <Text>{time}</Text>
+
       <Text>Current Temperature: {temperature} </Text>
       <Text>You should grab a {clothing} today!</Text>
     </View>
